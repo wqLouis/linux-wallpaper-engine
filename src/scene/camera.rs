c1cbe44601944ffe6ef9e9ee03664f97ebbcd993
wqLouis <wongqiluo@gmail.com>
2026-02-06 00:16:49 +0800

    feat(camera.rs): Projection struct

--- src/scene/camera.rs
+++ src/scene/camera.rs
@@ -1,5 +1,59 @@
+use glam::Vec3;
+
+use crate::scene::General;
+
 use super::Camera;
 
+pub struct Projection {
+    center: Vec3,
+    eye: Vec3,
+    up: Vec3,
+    aspect: f32,
+    nearz: f32,
+    farz: f32,
+    height: f32,
+    width: f32,
+    fov: f32,
+}
+
+pub struct WgpuCamera {
+    position: Vec3,
+    yaw: Vec3,
+    pitch: Vec3,
+}
+
 impl Camera {
-    pub fn new(&self) {}
+    pub fn new(&self, general: &General) -> Projection {
+        // Transform json camera read from json to Wgpu camera
+        let center = self.center.parse().unwrap_or_default();
+        let eye = self.eye.parse().unwrap_or_default();
+        let up = self.up.parse().unwrap_or_default();
+        let height: f32 = general.orthogonalprojection.height as f32;
+        let width: f32 = general.orthogonalprojection.width as f32;
+
+        Projection {
+            // Very rough may breaks
+            center: Vec3 {
+                x: center[0] as f32,
+                y: center[1] as f32,
+                z: center[2] as f32,
+            },
+            eye: Vec3 {
+                x: eye[0] as f32,
+                y: eye[1] as f32,
+                z: eye[2] as f32,
+            },
+            up: Vec3 {
+                x: up[0] as f32,
+                y: up[1] as f32,
+                z: up[2] as f32,
+            },
+            aspect: width / height,
+            width: width,
+            height: height,
+            nearz: general.nearz as f32,
+            farz: general.farz as f32,
+            fov: general.fov as f32,
+        }
+    }
 }
